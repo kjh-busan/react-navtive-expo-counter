e9d27011872d95115f70eb3f58aa62d2f9e0e39e
kjh-busan <picsmash7@gmail.com>
2023-02-24 01:22:38 +0900

    feat: WIP error

--- App.js
+++ App.js
@@ -1,26 +1,9 @@
-import React, {useState} from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
-
-import {styles} from './styles/styles';
+import React from 'react';
+import Counter from './src/Counter';
 
 export default function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <View style={styles.appContainer}>
-      <View style={styles.topContainer}>
-        <Text style={styles.textContainer}>Counter App : {count}</Text>
-      </View>
-      <View style={styles.bottomContainer}>
-        <TouchableOpacity onPress= {() => setCount(count - 1)}>
-          <Text style={styles.button}>+</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.bottomContainer}>
-        <TouchableOpacity onPress= {() => setCount(count + 1)}>
-          <Text style={styles.button}>-</Text>
-        </TouchableOpacity>
-      </View>
-    </View>    
+    <Counter />    
   );
 }
